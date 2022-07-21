ec2fbe9f2d37e8400a8f81578cdac0fba5743a5a
Kanat Tokonov <Kanat.Tokonov@KUMTOR.KG>
2022-07-21 11:15:29 +0600

    Person2

---
+++ src/main/java/com/example/Ankara1/Ankara1Application.java
@@ -0,0 +1,13 @@
+package com.example.Ankara1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ankara1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ankara1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Ankara1/controller/AnController.java
@@ -0,0 +1,13 @@
+package com.example.Ankara1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnController {
+    @GetMapping()
+    public String an(){
+        return "welcome Kana";
+    }
+}
