3d2423f0e3c03c6e7f0df22965252d4305e680dc
TokonovK <iksambuca@gmail.com>
2022-07-22 08:36:38 +0600

    0

--- src/main/java/com/example/Ankara1/controller/AnController.java
+++ src/main/java/com/example/Ankara1/controller/AnController.java
@@ -1,6 +1,4 @@
 package com.example.Ankara1.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
